c1377ae465142b237824b9c584b3110c11531cff
voinovanadia <voinovanadia@gmail.com>
2017-02-21 21:12:03 +0200

    Commit1

--- spec.js
+++ spec.js
@@ -1,5 +1,18 @@
-describe('', ()=> {
-    it('', ()=> {
-        expect(true).toBe(true)
+describe('Suite name', ()=> {
+    it('Test case name', ()=> {
+        //open browser
+        browser.get ('https://movies-finder.firebaseapp.com/',20 )
+        //search field is ElementFinder
+      let searchField = $("input[name='searchStr']")
+      let searchRequest = 'The Lord'
+      searchField.sendKeys(searchRequest)
+      element(by.buttonText('Go!')).click()
+
+      browser.sleep(5000)
+
+      let movieCard =$('movie-card')
+      let title = movieCard.$('h4 a').getText()
+        expect(title).toContain(searchRequest,('First serch result should contain Serch string'))
     })
+    
 })
